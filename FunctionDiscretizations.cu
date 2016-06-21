cfdb1a906d5354ac67ec93b7d952b9de36aaebf7
OSUmageed <mageed@oregonstate.edu>
2016-06-20 18:10:09 -0700

    I may have a handle on the K-S equation, we'll have to test it and see.

--- FunctionDiscretizations.cu
+++ FunctionDiscretizations.cu
@@ -2,3 +2,22 @@
 
 #include <cmath>
 #include <cstdlib>
+#include <cuda.h>
+
+
+// Maybe use float2 to store 2nd derivative with velocity.
+__device__ REAL Kuramoto_Sivashinsky(REAL uLeft, REAL uRight, REAL uCenter, REAL uxxLeft, REAL uxxRight, REAL uxxCenter)
+{
+
+    REAL cv = (uLeft*uLeft - uRight*uRight)/(4.f*dx);
+    REAL df = ((uLeft+uxxLeft)+(uRight+uxxRight)+(uCenter+uxxCenter))/(dx*dx);
+    return -(cv+df);
+
+}
+
+__device__ REAL Heat_Diffusion(REAL tLeft, REAL tRight, REAL tCenter)
+{
+
+    return fo*(tLeft+tRight) + (1.f-2.f*fo)*tCenter;
+
+}
