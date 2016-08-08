ec9fb604f44a0cc3adb613c3e544eb34181f9ca6
OSUmageed <mageed@oregonstate.edu>
2016-08-08 09:53:57 -0700

    No comment

--- Benchmarks/Interface.py
+++ Benchmarks/Interface.py
@@ -1,38 +1,27 @@
 #Just working on the GUI.
-
+#This is not the best way to do this, but Parts are working
 import os
 import sys
 import Tkinter as Tk
 
+#It's closer but a certain combination of them should make a certain menu appear.
 
-OPTIONS_A = [
-    "Shared",
-    "Registers"
-]
+OPTIONS_A = ["Shared", "Registers"]
 
-OPTIONS_O = [
-    "Plot Result",
-    "Performance Test"
-]
+OPTIONS_O = ["Plot Result", "Performance Test"]
 
-OPTIONS_P = [
-    "K-S",
-    "Heat",
-    "Euler"
-]
+OPTIONS_P = ["K-S", "Heat", "Euler"]
 
 def algorithm_choice(alg_sel):
 
     op_sel = Tk.StringVar(master)
     op_sel.set(OPTIONS_O[0]) # default value
 
-
-
     def operation_choice(op_sel):
 
         prob_sel = Tk.StringVar(master)
         prob_sel.set(OPTIONS_P[0]) # default value
-        problem_choice(prob_sel)
+
 
         def problem_choice(prob_sel):
 
@@ -43,22 +32,21 @@ def algorithm_choice(alg_sel):
                 checkme = "Hi"
 
             elif prob_sel == "Heat":
-                check_two = Tk.Checkbutton(master, text = "CPU/GPU sharing", variable = shared_proc)
+                check_two = Tk.Checkbutton(entryframe, text = "CPU/GPU sharing", variable = shared_proc)
                 check_two.pack(side = 'left')
 
             else:
-                check_two = Tk.Checkbutton(master, text = "CPU/GPU sharing", variable = shared_proc)
+                check_two = Tk.Checkbutton(entryframe, text = "CPU/GPU sharing", variable = shared_proc)
                 check_two.pack(side = 'left')
 
             print prob_sel, alg_sel, op_sel
 
-        problem_choice(prob_sel)
+        problem_menu = Tk.OptionMenu(dropframe, prob_sel, *OPTIONS_P, command = problem_choice)
+        problem_menu.pack(side = 'left')
+        
 
-        problem_menu = Tk.OptionMenu(master, prob_sel , *OPTIONS_P, command = problem_choice)
-        problem_menu.pack()
-
-    op_menu = Tk.OptionMenu(master, op_sel, *OPTIONS_O, command = operation_choice)
-    op_menu.pack()
+    op_menu = Tk.OptionMenu(dropframe, op_sel, *OPTIONS_O, command = operation_choice)
+    op_menu.pack(side = 'left')
 
 def ok():
     master.destroy()
@@ -70,12 +58,18 @@ def on_closing():
     raise SystemExit
 
 master = Tk.Tk()
+dropframe = Tk.Frame(master)
+entryframe = Tk.Frame(master)
+endframe = Tk.Frame(master)
+dropframe.pack()
+endframe.pack(side = 'bottom')
+entryframe.pack(side = 'bottom')              
 
 alg_sel = Tk.StringVar(master)
 alg_sel.set(OPTIONS_A[0]) # default value
 
-algol_menu = Tk.OptionMenu(master, alg_sel, *OPTIONS_A, command = algorithm_choice)
-algol_menu.pack()
+algol_menu = Tk.OptionMenu(dropframe, alg_sel, *OPTIONS_A, command = algorithm_choice)
+algol_menu.pack(side = 'left')
 
 plt_only = Tk.IntVar(master)
 plt_only.set(0)
@@ -93,10 +87,10 @@ arg4 = Tk.DoubleVar(master)
 arg4.set(2.0)
 
 master.protocol("WM_DELETE_WINDOW", on_closing)
-button = Tk.Button(master, text="OK", command=ok)
-button.pack()
-button = Tk.Button(master, text="Skip Run", command=skip)
-button.pack()
+button = Tk.Button(endframe, text="OK", command=ok)
+button.pack(side = "left")
+button = Tk.Button(endframe, text="Skip Run", command=skip)
+button.pack(side = "left")
 
 master.mainloop()
 
