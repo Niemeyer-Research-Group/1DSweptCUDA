a17aafc88167a416ac693503357331360c76b64e
OSUmageed <mageed@oregonstate.edu>
2016-09-10 10:12:55 -0700

    Concatenated the results.

--- SharedSwept/Results/perfAnalysis.py
+++ SharedSwept/Results/perfAnalysis.py
@@ -1,10 +1,7 @@
 import pandas as pd #use to html for storage?
 import numpy as np
-import subprocess as sp
-import shlex
 import os
 import os.path as op
-import sys
 import matplotlib.pyplot as plt
 
 #Gather files
@@ -15,10 +12,23 @@ for fl in os.listdir(thispath):
         files.append(fl)
 
 files = sorted(files)
-dfs = []
+dd = pd.read_table(files[0],delim_whitespace = True)
+headers = dd.columns.values.tolist()
+midx_name = ["Problem","Precision","Algorithm",headers[0]] 
+dfs_all = []
 #Put all files in dataframes.  Make a multiindex from names first.
 for f in files:
     dd = pd.read_table(f,delim_whitespace = True)
-    headers = dd.columns.values.tolist()
     ds = dd.pivot(headers[0],headers[1],headers[2])
-    dfs.append(ds)
+    idx1 = ds.index.get_level_values(0)
+    outidx = f.split("_")
+    midx = []
+    for i in idx1:
+        outidx[-1] = i 
+        midx.append(tuple(outidx))
+        
+    idx_real = pd.MultiIndex.from_tuples(midx,names=midx_name) 
+    ds.set_index(idx_real, inplace=True)
+    dfs_all.append(ds)
+    
+df_result = pd.concat(dfs_all)
